7a93cef2ef886c24873f3aa1e41a5e0888c65779
ChiaraMameli <mameli.chiara@libero.it>
2023-03-09 17:58:38 +0100

    header layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="container">
+  <div>
     <BaseHeader />
-    Hello World!
   </div>
 </template>
 
--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-  </div>
+  <header>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-2">Pippo</div>
+            <div class="col-2 text-center">Pippo</div>
+            <div class="col-2 d-flex justify-content-between">
+                <div>Ciccio</div>
+                <div>Ciccio</div>
+                <div>Ciccio</div>
+            </div>
+        </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,6 +20,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+
+header {
+    height: 5rem;
+    background-color: aquamarine;
+    padding: 1rem;
+}
 
 </style>
